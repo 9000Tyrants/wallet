6289522fd6802cae65e357af5551254ca2d5b2e2
9000Tyrants <144592009+9000Tyrants@users.noreply.github.com>
2025-05-19 09:06:02 +0530

    Create authController.js

---
+++ controller/authController.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    let user = await User.findOne({ username });
+    if (user) return res.status(400).json({ msg: 'User exists' });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+
+    user = new User({ username, password: hashed });
+    await user.save();
+
+    res.status(201).json({ msg: 'Registered' });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const token = jwt.sign({ user: { id: user._id } }, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
